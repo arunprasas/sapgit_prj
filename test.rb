2f3f3a11308a61ca3cccf6d55b93c91d864128ad
samarun <sam.arunprasad@gmail.com>
2024-01-21 10:19:34 +0530

    test.rb

---
+++ test.rb
@@ -0,0 +1,8 @@
+Git produce more output.
+  git require different phase to proceed.
+  such as working directory, staging area, repositry.
+  It says local repository and central repository.
+  It has its default branch namely master, it has its own pointer wheneven commit occur.
+  manually can create new branch but it will work under default branch master.
+
+  Lets make interesting topic
