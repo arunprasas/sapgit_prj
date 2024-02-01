0565b4a64d86781801852ce24722c0bc74119f04
samarun <sam.arunprasad@gmail.com>
2024-02-01 17:46:55 +0530

    commit san.py file

---
+++ san.py
@@ -0,0 +1,21 @@
+print("Hello Python World")
+def arun(name):
+    print(f'Hello, {name}') 
+
+if __name__ == '__main__':
+    arun('PyCharm')
+
+def arun(name):
+
+    print(f'Hi da, {name}')
+
+if __name__=='__main__':
+
+    arun('Welcome you')
+
+san = "Hello World"
+print(san)
+print(san[1])
+print(len(san))
+print(san.lower())
+print(san[2:5])
