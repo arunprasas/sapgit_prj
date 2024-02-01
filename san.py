a2695d8e14085dda86c5d2478008796bd5f583d1
samarun <sam.arunprasad@gmail.com>
2024-02-01 18:32:47 +0530

    san.py updated

--- san.py
+++ san.py
@@ -10,12 +10,21 @@ def arun(name):
     print(f'Hi da, {name}')
 
 if __name__=='__main__':
-
     arun('Welcome you')
-
 san = "Hello World"
 print(san)
 print(san[1])
 print(len(san))
 print(san.lower())
 print(san[2:5])
+
+ppl =['Arun','Prasad','Man','sun','software']
+san = [2,4,6,8,10]
+van=0
+for n in san:
+        if n%2==0:
+            van=+n
+            print("Printed number is even",+ van)
+for ss in ppl:
+            print("Name of List is :",ss)
+
